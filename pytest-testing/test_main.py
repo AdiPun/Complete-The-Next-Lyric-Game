eb5ef094a5483c443859ee6cd0c2a41cc5a51f20
Adi Pun <119054941+AdiPun@users.noreply.github.com>
2025-10-23 14:38:30 +0100

    Added exception testing

--- pytest-testing/test_main.py
+++ pytest-testing/test_main.py
@@ -1,6 +1,20 @@
-from main import get_weather
+from main import get_weather, add, divide
+import pytest
 
 def test_get_weather():
     assert get_weather(21) == "hot"
     assert get_weather(19) == "cold"
-    assert get_weather(20) == "hot"
+    assert get_weather(20) == "cold"
+
+def test_add():
+    assert add(2,3) == 5, "2 + 3 should be 5"
+    assert add(-1,1) == 0, "-1 + 1 should be 0"
+    assert add(0,0) == 0, "0 + 0 should be 0"
+    assert add(-1,-1) == -2, "-1 + -1 should be -2"
+
+def test_divide():
+    assert divide(4,2) == 2, "4 / 2 should be 2"
+    assert divide(5,2) == 2.5, "5 / 2 should be 2.5"
+    # tests if it raises an error
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(10,0)
