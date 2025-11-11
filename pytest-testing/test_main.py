0790862f2e82ada0e061969f539935c244322869
Adi Pun <119054941+AdiPun@users.noreply.github.com>
2025-11-11 09:55:35 +0000

    Add unit testing

--- pytest-testing/test_main.py
+++ pytest-testing/test_main.py
@@ -1,6 +1,19 @@
 from main import get_weather, add, divide
 import pytest
 
+class UserManager:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self,username,email):
+        if username in self.users:
+            raise ValueError("User already exists")
+        self.users[username] = email
+        return True
+    
+    def get_user(self,username):
+        return self.users.get(username)
+
 def test_get_weather():
     assert get_weather(21) == "hot"
     assert get_weather(19) == "cold"
@@ -15,6 +28,6 @@ def test_add():
 def test_divide():
     assert divide(4,2) == 2, "4 / 2 should be 2"
     assert divide(5,2) == 2.5, "5 / 2 should be 2.5"
-    # tests if it raises an error
+    # tests if it raises an error when function is executed
     with pytest.raises(ValueError, match="Cannot divide by zero"):
         divide(10,0)
